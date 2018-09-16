996772d4610caadfb974ed4b8b9cd3f411a5e54a
Eric Groom <bcgroom@gmail.com>
2018-09-16 11:53:10 -0700

    configure webpack/babel and add some basic components to test

---
+++ frontend/src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'React'
+import {Link} from 'react-router-dom'
+
+import './Nav.css'
+
+const Nav = () => {
+    return (
+        <nav>
+            <ul className='nav-list'>
+                <li className='nav-list-item'><Link to="/">home</Link></li>
+                <li className='nav-list-item'><Link to="/about">about</Link></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
