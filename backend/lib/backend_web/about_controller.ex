a123ded6c7b9531ab8816b4d9ef4b898b3dfa4c2
Eric Groom <bcgroom@gmail.com>
2018-09-19 20:03:48 -0700

    basic page for Eric

--- backend/lib/backend_web/about_controller.ex
+++ backend/lib/backend_web/about_controller.ex
@@ -11,18 +11,14 @@ defmodule BackendWeb.AboutController do
  end
 
   def jonas(conn, _) do
-  response = %{
-  ABOUT: "This is the Jonas about page."
-  }
-
-  json(conn, response)
-  end
-  def eric(conn, _) do
-  response = %{
-    ABOUT: "This is the Eric about page."
+    response = %{
+    ABOUT: "This is the Jonas about page."
     }
 
     json(conn, response)
+  end
+  def eric(conn, _) do
+    render conn, "eric.html"
   end
   def mariko(conn, _) do
   response = %{
