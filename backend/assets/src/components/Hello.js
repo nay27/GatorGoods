ae3aadc1ad045e4c27e740adc680452aec4979e4
Eric Groom <bcgroom@gmail.com>
2018-10-16 18:49:38 -0700

    add styled components

---
+++ backend/assets/src/components/Hello.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Card from "../components/styles/Card"
+
+const Hello = () => (
+    <Card>
+        <h1 className="hi">Hi from react!</h1>
+    </Card>
+);
+
+export default Hello;
--- backend/assets/src/pages/home.js
+++ backend/assets/src/pages/home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => (<div>Hello from React :)</div>);
+import Hello from "../components/Hello";
+
+const App = () => (<div><Hello /></div>);
 
 ReactDOM.render(<App />, document.getElementById("react-root"));
