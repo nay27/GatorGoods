de60e0f478558d2b830262262540ec1af2f7d3f3
Eric Groom <bcgroom@gmail.com>
2018-10-26 14:58:36 -0700

    Merge with develop

--- backend/lib/backend/users/item.ex
+++ backend/lib/backend/users/item.ex
@@ -19,6 +19,6 @@ defmodule Backend.Users.Item do
     item
     |> cast(attrs, [:title, :description, :price, :status])
     |> validate_required([:title, :description, :price, :status])
-    |> cast_assoc(:categories)
+    |> cast_assoc(:category)
   end
 end
