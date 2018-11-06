d08445f4bc515cc2490b7a1007688afb1a7c987d
Eric Groom <bcgroom@gmail.com>
2018-11-05 19:40:10 -0800

    add cors

--- backend-api/goodsrest/goodsrest/settings.py
+++ backend-api/goodsrest/goodsrest/settings.py
@@ -27,6 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_WHITELIST = [
+    "localhost:3000",
+    "52.53.153.2"
+]
+
 
 # Application definition
 
@@ -38,11 +43,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
