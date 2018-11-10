7bfc8a6827dd31cc7ddca20d31634a1b865c3a4f
Muhammad Vaaceph Khan <muhammad.vaaceph@gmail.com>
2018-11-10 16:53:05 +0100

    Location Model

--- backend-api/goodsrest/goods/models.py
+++ backend-api/goodsrest/goods/models.py
@@ -1,1 +1,6 @@
 from django.db import models
+
+class locations(models.Model):
+    lid = models.IntegerField(max_length=50, primary_key=True)
+    name = models.CharField(max_length=50)
+    map = models.CharField(max_length=4096)
