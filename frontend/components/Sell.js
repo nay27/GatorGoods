cb7ec99e90f35a5f4447f3cf16b830b56aa4b61f
Eric Groom <bcgroom@gmail.com>
2018-11-13 15:13:16 -0800

    sell styling

---
+++ frontend/components/Sell.js
@@ -0,0 +1,53 @@
+import Form from "./styles/Form";
+
+class Sell extends React.Component {
+  state = {
+    loading: false,
+    title: "",
+    description: "",
+    price: 0,
+    category: 0,
+    error: null
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+  }
+
+  render() {
+    return (
+      <Form>
+        <fieldset>
+          <h1> Sell Item </h1>
+          <label htmlFor="title">
+            Title*
+            <input type="text" placeholder="Title Text" name="title" />
+          </label>
+          <label htmlFor="category">
+            Category*
+            <br />
+            <select name="category">
+              <option value="1">Furniture</option>
+              <option value="2">Electronics</option>
+              <option value="3">Clothing</option>
+              <option value="4">Books</option>
+            </select>
+          </label>
+          <label htmlFor="Price">
+            Price*
+            <input type="text" placeholder="$" />
+          </label>
+          <label htmlFor="description">
+            Description*
+            <input type="text" placeholder="Description" />
+          </label>
+          <label htmlFor="image">
+            Image <input type="file" name="image" />
+          </label>
+        </fieldset>
+      </Form>
+    );
+  }
+}
+
+export default Sell;
