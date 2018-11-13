e50a86bd47a2d23c0527a60a3bb11b73295dfdcb
Eric Groom <egroom@mail.sfsu.edu>
2018-11-12 16:39:38 -0800

    create basic signup page and form styles

---
+++ frontend/pages/signin.js
@@ -0,0 +1,12 @@
+import Signin from "../components/Signin";
+import Signup from "../components/Signup";
+import Grid from "../components/styles/Grid";
+
+const SigninPage = () => (
+  <Grid>
+    <Signin />
+    <Signup />
+  </Grid>
+);
+
+export default SigninPage;
