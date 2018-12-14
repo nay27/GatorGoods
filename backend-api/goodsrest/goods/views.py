add209f6b5c50886e3fe9c08b4ad42120ce78a47
Abdullah Al Zakir Hossain <aazh@eeloy.com>
2018-12-14 13:31:10 +0100

    Disabled CSRF token verification.

--- backend-api/goodsrest/goods/views.py
+++ backend-api/goodsrest/goods/views.py
@@ -105,3 +105,11 @@ class FileUploadView(APIView):
 
         return Response(status=status.HTTP_201_CREATED)
 
+class DisableCSRFMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        setattr(request, '_dont_enforce_csrf_checks', True)
+        response = self.get_response(request)
+        return response
